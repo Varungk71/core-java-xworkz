5942dd7db25762a699148eb0f0c26cfb9f4426b2
Varungk71 <vgk7165@gmail.com>
2023-07-13 14:31:33 +0530

    adding method overloading files

---
+++ InstagramTester.java
@@ -0,0 +1,16 @@
+class InstagramTester{
+
+	public static void main(String args[]){
+	
+	Instagram.login(8431017474L,"Varungk@8431");
+	
+	Instagram.login(7431017474L,"Varungk@8431");
+	
+	Instagram.login(8431017474L,"Varungk");
+	
+	Instagram.login("varungk@gmail.com","Varungk@8431");
+	
+	Instagram.login("varun@gmail.com","Varungk@8431");
+	
+	}
+}
--- ZomatoTester.java
+++
@@ -1,9 +0,0 @@
-class ZomatoTester{
-
-	public static void main(String food[]){
-	
-	String foodname="pizza";
-	double foodPrice =Zomato.search(foodname);
-		System.out.println("food price is " + foodPrice);
-	}
-}
