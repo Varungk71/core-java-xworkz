8604de3f7dff0989b589cf9d1c0c76565ae035bb
Varungk71 <vgk7165@gmail.com>
2023-08-29 14:57:50 +0530

    adding absraction file

---
+++ library-management-system/src/com/xworkz/library/abstraction/Library.java
@@ -0,0 +1,24 @@
+package com.xworkz.library.abstraction;
+
+import com.xworkz.library.constant.BookType;
+import com.xworkz.library.dto.BookDTO;
+
+public interface Library
+{
+    public boolean addBook(BookDTO dto);
+    public void getAllBooks();
+    public String getBookNameByBookid(int bookId);
+    public BookDTO[] getBookByYear(int yearOfPublication);
+    public BookDTO[] getBookByBookAuthor(String author);
+    public BookDTO getBookByBookPublication(String publication);
+    public String getBooknameByBookType(BookType type);
+    public double getBookPriceByBookName(String bookName);
+    public String getBooknameByYearOfPublication(int bookId);
+
+    public boolean updateBookPageByBookId(int UpdatedPage , int bookId);
+    public boolean updateBookPriceByBookId(int UpdatedPrice,int bookId);
+
+    public boolean updatePublisherByAuthor(String existingAuthor,String updatedPublisher);
+    public boolean deleteBookByBookId(int bookId);
+
+}
---
+++ library-management-system/src/com/xworkz/library/dto/BookDTO.java
@@ -0,0 +1,24 @@
+package com.xworkz.library.dto;
+
+import com.xworkz.library.constant.BookType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+
+public class BookDTO
+{
+
+    private int bookId;
+    private String bookName;
+    private String bookAuthor;
+    private BookType type;
+    private int noOfPage;
+    private String publication;
+    private double price;
+    private int publishedYear;
+
+}
